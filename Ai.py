534a83c2a2c53be9eb201b8b5cc05f28673193c5
TallKid420 <94267055+TallKid420@users.noreply.github.com>
2022-12-17 13:58:01 -0500

    better

--- Ai.py
+++ Ai.py
@@ -1,6 +1,11 @@
-import pygame, os, nltk, json, speech_recognition as sr
+import pygame, os, nltk, json, speech_recognition as sr, re, requests, nltk, numpy as np, networkx as nx
 from Actions import *
 from gtts import gTTS
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+from nltk.cluster.util import cosine_distance
+from sklearn.feature_extraction.text import CountVectorizer
+
 
 JSONFILE = ""
 r = sr.Recognizer()
@@ -51,35 +56,88 @@ def choose_action(action):
     else:
         Speak("There is no action for that. Please Email evannriley7@gmail.com to add it")
 
+def scrape_info(topic):
+    # Set up the list of websites to scrape
+    websites = [
+        "https://en.wikipedia.org/wiki/{}".format(topic)
+    ]
+
+    # Initialize an empty list to store the results
+    results = []
+
+    # Iterate over each website
+    for website in websites:
+        # Make a request to the website and retrieve the HTML content
+        html_doc = requests.get(website).text
+        # Parse the HTML using BeautifulSoup
+        soup = BeautifulSoup(html_doc, 'html.parser')
+        # Extract the relevant information from the website
+        info = soup.get_text()
+        # Add the information to the list of results
+        results.extend(info)
+        results = ''.join(results)
+    # Return the list of results
+    return results
+
+def remove_non_text(input_string):
+  # Use a regular expression to match any character that is not a letter or a space
+  pattern = re.compile(r'[^a-zA-Z\s\.]')
+  # Replace all non-letter and non-space characters with an empty string
+  output_string = pattern.sub('', input_string)
+  return output_string
+
+def summarize(text, question):
+    x=0
+    lq = question.split(' ')
+    for i in lq:
+        # logic to decide what it is asking
+
+        # what is a () logic
+        if i.lower() == "what" and lq[x+1] == "is" and lq[x+2] == "an" or i.lower() == "what" and lq[x+1] == "is" and lq[x+2] == "a":
+            pattern = r'(.*\b(an '+lq[x+3]+r' is)\b.*)'
+            match = re.search(pattern, text.lower())
+            matches = re.findall(pattern, text.lower())
+            print(matches)
+        x=x+1
+
+def fallback_response(question):
+    text = scrape_info("Apple")
+    text = remove_non_text(text)
+    summary = summarize(text, question)   
+    print(summary)
+
 def Respond_To_Question(question):
     qs = detect_subject(question)
     if qs[1]:
-        q = qs[0]
-        word_counts = {}
-        for word in q:
-            if word in word_counts:
-                word_counts[word] += 1
-            else:
-                word_counts[word] = 1
-        most_common_word = max(word_counts, key=word_counts.get)
-        max_count = word_counts[most_common_word]
-        tied_words = [word for word, count in word_counts.items() if count == max_count]
-        if len(tied_words) > 1:
-            Speak("Which of the following did you want, {}".format(' '.join(tied_words)))
-            f=False
-            c=''
-            inp = get_input()
-            for n in tied_words:
-                if inp == n:
-                    f=True
-                    c=n
-            if(f):
-                choose_action(c)
-            else:
-                Speak("That is not A option")
-                Respond_To_Question(question)
+        if qs[0] == "":
+            fallback_response(question)
         else:
-            choose_action(most_common_word)
+            q = qs[0]
+            word_counts = {}
+            for word in q:
+                if word in word_counts:
+                    word_counts[word] += 1
+                else:
+                    word_counts[word] = 1
+            most_common_word = max(word_counts, key=word_counts.get)
+            max_count = word_counts[most_common_word]
+            tied_words = [word for word, count in word_counts.items() if count == max_count]
+            if len(tied_words) > 1:
+                Speak("Which of the following did you want, {}".format(' '.join(tied_words)))
+                f=False
+                c=''
+                inp = get_input()
+                for n in tied_words:
+                    if inp == n:
+                        f=True
+                        c=n
+                if(f):
+                    choose_action(c)
+                else:
+                    Speak("That is not A option")
+                    Respond_To_Question(question)
+            else:
+                choose_action(most_common_word)
     else:
         for i in JSONFILE:
             if i == qs:
@@ -89,9 +147,9 @@ update_JSON()
 while True:
     # Listen for audio
     with mic as source:
+        print("Listening")
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source)
-
     # Transcribe the audio to text
     transcription = r.recognize_google(audio)
 
--- AiWrittenByAi.py
+++ AiWrittenByAi.py
@@ -1,33 +1,59 @@
+import re
+import requests
+from bs4 import BeautifulSoup
 import nltk
+from nltk.corpus import stopwords
+from nltk.cluster.util import cosine_distance
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+import networkx as nx
 
-def process_input(input_str):
-  # Tokenize the input string
-  tokens = nltk.word_tokenize(input_str)
-  
-  # Tag the tokens with their part of speech
-  pos_tags = nltk.pos_tag(tokens)
-  
-  # Determine the intent of the input
-  intent = None
-  if "greet" in pos_tags:
-    intent = "greeting"
-  elif "thank" in pos_tags:
-    intent = "thanking"
-  elif "goodbye" in pos_tags:
-    intent = "goodbye"
-  
-  # Execute the appropriate action based on the intent
-  response = ""
-  if intent == "greeting":
-    response = "Hello! How can I help you today?"
-  elif intent == "thanking":
-    response = "You're welcome! Is there anything else I can do for you?"
-  elif intent == "goodbye":
-    response = "Goodbye! Have a great day."
-  
-  return response
+def remove_non_text(input_string):
+  # Use a regular expression to match any character that is not a letter or a space
+  pattern = re.compile(r'[^a-zA-Z\s\.]')
+  # Replace all non-letter and non-space characters with an empty string
+  output_string = pattern.sub('', input_string)
+  return output_string
 
-# Test the process_input function
-input_str = "Hi, I just wanted to say thank you for your help."
-output = process_input(input_str)
-print(output) # "Hello! How can I help you today?"
+def summarize(text, question):
+    x=0
+    lq = question.split(' ')
+    for i in lq:
+        # logic to decide what it is asking
+
+        # what is a () logic
+        if i.lower() == "what" and lq[x+1] == "is" and lq[x+2] == "an" or i.lower() == "what" and lq[x+1] == "is" and lq[x+2] == "a":
+            pattern = r'(.*\b(an '+lq[x+3]+r' is)\b.*)'
+            match = re.search(pattern, text.lower())
+            matches = re.findall(pattern, text.lower())
+            print(matches)
+        x=x+1
+            
+
+def scrape_info(topic):
+    # Set up the list of websites to scrape
+    websites = [
+        "https://en.wikipedia.org/wiki/{}".format(topic)
+    ]
+
+    # Initialize an empty list to store the results
+    results = []
+
+    # Iterate over each website
+    for website in websites:
+        # Make a request to the website and retrieve the HTML content
+        html_doc = requests.get(website).text
+        # Parse the HTML using BeautifulSoup
+        soup = BeautifulSoup(html_doc, 'html.parser')
+        # Extract the relevant information from the website
+        info = soup.get_text()
+        # Add the information to the list of results
+        results.extend(info)
+        results = ''.join(results)
+    # Return the list of results
+    return results
+
+text = scrape_info("Apple")
+text = remove_non_text(text)
+summary = summarize(text, "what is an apple")
+print(summary)
